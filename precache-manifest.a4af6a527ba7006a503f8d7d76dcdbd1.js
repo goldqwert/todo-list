bf6e148c9f0aa2a84ac46c9a29c3036de2435e3e
Yury <goldyukol12@gmail.com>
2020-01-29 14:18:18 +0200

    Updates

---
+++ precache-manifest.a4af6a527ba7006a503f8d7d76dcdbd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df7522c9775362e5500d0dfbde7889d",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "19b823e7c85927e352a3",
+    "url": "/TodoList/static/css/main.cb2505cd.chunk.css"
+  },
+  {
+    "revision": "27e9e70a71d43c6b72df",
+    "url": "/TodoList/static/js/2.ddf5db45.chunk.js"
+  },
+  {
+    "revision": "19b823e7c85927e352a3",
+    "url": "/TodoList/static/js/main.bf31bcd0.chunk.js"
+  },
+  {
+    "revision": "23ef1ee30c4a1e2cab36",
+    "url": "/TodoList/static/js/runtime~main.90b1048d.js"
+  }
+]);
